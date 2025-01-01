ab902ba236b75f4cb8e3acd84236c67f09f79f70
권정훈 <righthun.kwon@gmail.com>
2025-01-01 11:57:30 +0900

    change order

--- server/server.js
+++ server/server.js
@@ -3,15 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// middleware setting
 app.use(express.static(path.join(__dirname, '..')));
-
-// always return index.html
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
-
-// line request
 app.listen(PORT, () => {
   console.log(`START SERVER`);
 });
